c049fb511f24ab83fe638ec5f24ba83276d92f0d
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-12 05:17:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "anthony";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "not anthony";
+}
+// changeLeastFavoriteCustomer();
+// TypeError: Assignment to constant variable.
